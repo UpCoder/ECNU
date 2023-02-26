fa9abfc5f9cc3fa7d28ae5a43abca8dfb295d267
liangdong <546043882@qq.com>
2023-02-26 16:49:21 +0800

    release bat

---
+++ 启动GPTv2.bat
@@ -0,0 +1,6 @@
+E:
+cd E:\\PycharmProjects\\ECNU\\demo\\demo_asr
+call conda activate base
+set PYTHONPATH=E:\\PycharmProjects\\ECNU;E:\\PycharmProjects\\ECNU\\src\\body
+python qa_pipeline_v2.py
+pause
